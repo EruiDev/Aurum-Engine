4e778ba44aeae5a6d04432098af541abf756e4d9
Oriol Moles <molesoriol@gmail.com>
2026-03-26 14:52:48 +0100

    add: All metrics I want to check for

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,49 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	PaymentsCreatedTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "aurum_payments_created_total",
+			Help: "Total number of payments created by currency",
+		},
+		[]string{"currency"},
+	)
+
+	PaymentTransitionTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "aurum_payment_transition_total",
+			Help: "Total number of transitions on payment states",
+		},
+		[]string{"from", "to"},
+	)
+
+	OutboxPendingEvents = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "aurum_outbox_pending_events",
+			Help: "Current number of unpublished outbox events.",
+		},
+	)
+
+	HTTPRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "aurum_http_request_duration_seconds",
+			Help:    "HTTP request duration in seconds.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	DBQueryDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "aurum_db_query_duration_seconds",
+			Help:    "Database query duration in seconds.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"operation"},
+	)
+)
